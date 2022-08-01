62d1c2035bf95b8f0b06968aa1c065e974ca982c
sammyach <sammyachman@gmail.com>
2022-08-01 19:58:14 +0000

    added n configured particles.js

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+declare var particlesJS: any;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'portfolio';
   loading = true;
+
   constructor(){
     setTimeout(()=>{ this.loading = false},4000)
   }
+  ngOnInit(): void {
+    particlesJS.load('particles-js', '../assets/particles.json', null);
+  }
+
 }
